361c2693ccc663715a4c3d0d0f27350e88228f2f
김태혁 <67897827+kimtaehyuk1@users.noreply.github.com>
2023-07-29 01:12:18 +0900

    doc: 풀었던 문제 반복

--- 자료구조/카드2.py
+++ 자료구조/카드2.py
@@ -5,14 +5,18 @@
 from collections import deque
 
 
+# 시간 복잡도를 따져야되
+
+
 N = int(input())
-# dq = deque()
+dq = deque()
 # deque에 1~N까지 담기
-# for i in range(1,N+1):
-#     deque.append(i)
+for i in range(1,N+1):
+    dq.append(i)
 # 선언 할때 부터 담아보기
-dq = deque(range(1,N+1))
-#print(dq)
+#dq = deque(range(1,N+1))
+
+
 # dq의 길이가 1보다 크면 두 행위(queue로 앞에꺼 제거, 남은 맨앞 제거후 뒤에 삽입!) 반복
 while len(dq) > 1:
     dq.popleft()
