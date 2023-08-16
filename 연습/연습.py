b163dd1b6b2fa5da865c78009ea3f43d7b1f0d96
김태혁 <67897827+kimtaehyuk1@users.noreply.github.com>
2023-08-17 01:30:24 +0900

    feat: 코테 문제풀이

--- 연습/연습.py
+++ 연습/연습.py
@@ -42,11 +42,41 @@
 # dy = (0,1,0,-1)
 # print(dy[-2])
 
-numbers = [1, 2, 4, 5]
+# numbers = [1, 2, 4, 5]
 
-for num in numbers:
-    if num == 3:
-        print("숫자 3을 찾았습니다.")
-        break
-else:
-    print("숫자 3을 찾지 못했습니다.")
+# for num in numbers:
+#     if num == 3:
+#         print("숫자 3을 찾았습니다.")
+#         break
+# else:
+#     print("숫자 3을 찾지 못했습니다.")
+
+# for i in range(5,0,-1):
+#     print(i, end='')
+
+# sequence = [1, 1, 1, 2, 3, 4, 5]
+# k = 5
+# answers = []
+# sum = 0
+# l = 0
+# r = -1
+
+# while True:
+#     if sum < k:
+#         r += 1
+#         if r >= len(sequence):
+#             break
+#         sum += sequence[r]
+#     else:
+#         sum -= sequence[l]
+#         if l >= len(sequence):
+#             break
+#         l += 1
+#     if sum == k:
+#         answers.append([l, r])
+
+# print(answers)
+
+ans = [[0, 3], [4, 4], [6, 6]]
+ans = sorted(ans, key=lambda x: (x[1]-x[0], x[0]))
+print(ans)
